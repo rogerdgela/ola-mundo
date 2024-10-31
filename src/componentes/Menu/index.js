7989a11c6caa7389a0a934eb59ecbefc7c61441b
Rogerio Silva <dgelask8@gmail.com>
2024-10-31 17:04:20 -0300

    Utilizando useLocation

--- src/componentes/Menu/index.js
+++ src/componentes/Menu/index.js
@@ -1,16 +1,18 @@
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 import styles from "./Menu.module.css"
+import MenuLink from "../MenuLink"
 
 export default function Menu() {
     return (
         <header>
             <nav className={styles.navegacao}>
-                <Link className={styles.link} to="/">
+                <MenuLink to="/">
                     Inicio
-                </Link>
-                <Link className={styles.link} to="/sobre-mim">
-                    Sobre min
-                </Link>
+                </MenuLink>
+
+                <MenuLink to="/sobre-mim">
+                    Sobre Mim
+                </MenuLink>
             </nav>
         </header>
     )
--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import Inicio from "./paginas/Inicio";
 import SobreMim from "./paginas/SobreMim";
 import Menu from "./componentes/Menu";
 
-function App() {
+function AppRoutes() {
     return (
         <BrowserRouter>
             <Menu />
@@ -16,4 +16,4 @@ function App() {
     )
 }
 
-export default App;
+export default AppRoutes;
