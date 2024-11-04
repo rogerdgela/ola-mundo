ffdc6b230dd47f6df83ba1120fd64af648e68b63
Rogerio Silva <dgelask8@gmail.com>
2024-11-04 16:50:21 -0300

    Utilizando rotas aninhadas

--- src/routes.js
+++ src/routes.js
@@ -3,15 +3,19 @@ import Inicio from "./paginas/Inicio";
 import SobreMim from "./paginas/SobreMim";
 import Menu from "./componentes/Menu";
 import Rodape from "componentes/Rodape";
+import PaginaPadrao from "componentes/PaginaPadrao";
 
 function AppRoutes() {
     return (
         <BrowserRouter>
             <Menu />
-            
+
             <Routes>
-                <Route path="/" element={<Inicio />} />
-                <Route path="/sobre-mim" element={<SobreMim />} />
+                <Route path="/" element={<PaginaPadrao />}>
+                    <Route path="/" element={<Inicio />} />
+                    <Route path="/sobre-mim" element={<SobreMim />} />
+                </Route>
+
                 <Route path="*" element={<div>Página não encontrada</div>} />
             </Routes>
 
