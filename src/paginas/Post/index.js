8e6a9122b907de3f89ea96b1d1141c2febc771f4
Rogerio Silva <roger.alves.silva84@gmail.com>
2024-11-07 16:22:36 -0300

    Utilizando useParams

---
+++ src/paginas/Post/index.js
@@ -0,0 +1,9 @@
+import { useParams } from "react-router-dom"
+
+export default function Post() {
+    const parametros = useParams();
+
+    return (
+        <h1>Post {parametros.id}</h1>
+    )
+}
