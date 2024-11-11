d8525a1ae6ae36a3bb13ab769d2995239ea14932
Motors BR <dgelask8@gmail.com>
2024-11-11 17:27:29 -0300

    Utilizando useNavigate

--- src/paginas/NaoEncontrada/index.js
+++ src/paginas/NaoEncontrada/index.js
@@ -1,36 +1,39 @@
+import BotaoPrincipal from "componentes/BotaoPrincipal";
 import styles from "./NaoEncontrada.module.css";
 import erro404 from "assets/erro_404.png";
+import { useNavigate } from "react-router-dom";
 
 export default function NaoEncontrada() {
-    return (
-        <>
-            <div className={styles.conteudoContainer}>
-                <span className={styles.texto404}>404</span>
+  const navegar = useNavigate();
 
-                <p className={styles.titulo}>
-                    Ops! Página não encontrada.
-                </p>
+  return (
+    <>
+      <div className={styles.conteudoContainer}>
+        <span className={styles.texto404}>404</span>
 
-                <p className={styles.paragrado}>
-                    Tem certeza de que era isso que você estava procurando? 
-                </p>
+        <p className={styles.titulo}>Ops! Página não encontrada.</p>
 
-                <p className={styles.paragrado}>
-                    Aguarde uns instantes e recarregue a página, ou volte para a página inicial.
-                </p>
+        <p className={styles.paragrado}>
+          Tem certeza de que era isso que você estava procurando?
+        </p>
 
-                <div className={styles.botaoContainer}>
-                    <button>Voltar</button>
-                </div>
+        <p className={styles.paragrado}>
+          Aguarde uns instantes e recarregue a página, ou volte para a página
+          inicial.
+        </p>
 
-                <img 
-                    className={styles.imagemCachorro}
-                    src={erro404}
-                    alt="Cachorro de oculos e vestido como humano"
-                />
-            </div>
+        <div className={styles.botaoContainer} onClick={() => navegar(-1)}>
+          <BotaoPrincipal tamanho="lg">Voltar</BotaoPrincipal>
+        </div>
 
-            <div className={styles.espacoEmBranco}></div> 
-        </>
-    )
+        <img
+          className={styles.imagemCachorro}
+          src={erro404}
+          alt="Cachorro de oculos e vestido como humano"
+        />
+      </div>
+
+      <div className={styles.espacoEmBranco}></div>
+    </>
+  );
 }
